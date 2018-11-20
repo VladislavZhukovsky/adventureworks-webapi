bc724263aeaf63ff3c22242b0ebfd95d1ade4e3b
Владислав Жуковский <jaymzmanunited@gmail.com>
2018-11-20 13:44:56 +0300

    Refactoring for AzureStorageClient library

---
+++ source/AdventureWorks/AdventureWorks.AzureStorageClient/AzureStorageClient.cs
@@ -0,0 +1,39 @@
+﻿using AdventureWorks.AzureStorageClient.Clients;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.AzureStorageClient
+{
+    public class AzureStorageClient
+    {
+        private readonly CloudStorageAccount storageAccount;
+        private LoggerClient loggerClient;
+        private BlobClient blobClient;
+        private MessageQueueClient messageQueueClient;
+
+        public AzureStorageClient()
+        {
+            storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            loggerClient = new LoggerClient(storageAccount);
+            blobClient = new BlobClient(storageAccount);
+            messageQueueClient = new MessageQueueClient(storageAccount);
+        }
+
+        public void InitializeLoggerClient()
+        {
+        }
+
+        public void InitializeBlobClient()
+        {
+        }
+
+        public void InitializeMessageQueueClient()
+        {
+        }
+    }
+}
---
+++ source/AdventureWorks/AdventureWorks.AzureStorageClient/Clients/LoggerClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.AzureStorageClient.Clients
+{
+    public class LoggerClient
+    {
+        private readonly Serilog.Core.Logger logger;
+
+        public LoggerClient(CloudStorageAccount storageAccount)
+        {
+            logger = new LoggerConfiguration()
+                .WriteTo
+                .AzureTableStorageWithProperties(storageAccount, storageTableName: ConfigurationManager.AppSettings["StorageTableName"])
+                .CreateLogger();
+        }
+
+        public void Info(string message)
+        {
+            logger.Information(message);
+        }
+
+        public void Error(string message, Exception ex = null)
+        {
+            if (ex == null)
+            {
+                logger.Error(message);
+            }
+            else
+            {
+                logger.Error(ex, message);
+            }
+        }
+    }
+}
